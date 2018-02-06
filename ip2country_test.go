15a725abd733e7ac692f6068568450b8d644eba3
asgari <asgari@inside.sahab.ir>
2018-02-06 15:19:35 +0330

    Remove creating file manually in test

--- ip2country_test.go
+++ ip2country_test.go
@@ -1,84 +1,52 @@
 package ip2country
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 func TestIPtoCountryLookup(t *testing.T) {
-	file, err := ioutil.TempFile("", "ip2country")
-	if err != nil {
-		t.Fatal(err)
-	}
-	file.Write([]byte(`"0.0.0.0","0.0.0.255","AA"`))
-	file.Write([]byte("\n"))
-	file.Write([]byte(`"0.0.32.0","0.0.64.255","BB"`))
-	file.Write([]byte("\n"))
-	file.Write([]byte(`"0.128.0.0","0.128.255.255","CC"`))
-	file.Write([]byte("\n"))
-	file.Write([]byte(`"80.0.0.0","90.0.0.0","DD"`))
-	file.Write([]byte("\n"))
-	file.Write([]byte(`"91.0.0.0","255.255.255.255","EE"`))
-	err = file.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(file.Name())
 
-	err = Load(file.Name())
+	err := Load("./dbip-country.csv")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	country := GetCountry("0.0.0.1")
-	if country != "AA" {
-		t.Errorf("Expected AA but found %s", country)
+	country := GetCountry("0.0.0.0")
+	if country != "ZZ" {
+		t.Errorf("Expected ZZ but found %s", country)
+	}
+	country = GetCountry("0.0.0.1")
+	if country != "ZZ" {
+		t.Errorf("Expected ZZ but found %s", country)
+	}
+	country = GetCountry("0.255.255.255")
+	if country != "ZZ" {
+		t.Errorf("Expected ZZ but found %s", country)
 	}
 
-	country = GetCountry("0.0.0.255")
-	if country != "AA" {
-		t.Errorf("Expected AA but found %s", country)
+	country = GetCountry("50.97.196.208")
+	if country != "US" {
+		t.Errorf("Expected US but found %s", country)
 	}
 
-	country = GetCountry("0.0.32.0")
-	if country != "BB" {
-		t.Errorf("Expected BB but found %s", country)
+	country = GetCountry("50.97.198.135")
+	if country != "CN" {
+		t.Errorf("Expected US but found %s", country)
 	}
 
-	country = GetCountry("0.0.54.100")
-	if country != "BB" {
-		t.Errorf("Expected BB but found %s", country)
+	country = GetCountry("50.97.198.135")
+	if country != "CN" {
+		t.Errorf("Expected US but found %s", country)
 	}
 
-	country = GetCountry("0.128.1.1")
-	if country != "CC" {
-		t.Errorf("Expected CC but found %s", country)
+	country = GetCountry("200.95.185.34")
+	if country != "CL" {
+		t.Errorf("Expected CL but found %s", country)
 	}
 
-	country = GetCountry("1.0.0.0") //not in a database
-	if country != "" {
-		t.Errorf("Expected NOTHING but found %s", country)
-	}
-
-	country = GetCountry("85.128.129.130")
-	if country != "DD" {
-		t.Errorf("Expected DD but found %s", country)
-	}
-
-	country = GetCountry("91.0.0.0")
-	if country != "EE" {
-		t.Errorf("Expected EE but found %s", country)
-	}
-
-	country = GetCountry("92.93.94.95")
-	if country != "EE" {
-		t.Errorf("Expected EE but found %s", country)
-	}
-
-	country = GetCountry("255.255.255.255")
-	if country != "EE" {
-		t.Errorf("Expected EE but found %s", country)
+	country = GetCountry("223.255.255.255")
+	if country != "AU" {
+		t.Errorf("Expected AU but found %s", country)
 	}
 }
 
